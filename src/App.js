5252e24fef29ecb4c5c993e138f22b932852a27e
kanginhan <hunt2433@naver.com>
2020-06-11 19:26:11 +0900

    dev

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [list, setList] = useState([{
+    name: "홍길동",
+    phone: "000000",
+  }]);
+
+  const initialData = {
+    name: "",
+    phone: "",
+  };
+  const [data, setData] = useState(initialData);
+  const [keyword, setKeyword] = useState("");
+
+  const addContact = () => {
+    setList([
+      ...list,
+      data
+    ]);
+    setData(initialData);
+  }
+
+  const changeName = (e) => {
+    setData({
+      ...data,
+      name: e.target.value,
+    });
+  }
+
+  const changePhone = (e) => {
+    setData({
+      ...data,
+      phone: e.target.value,
+    });
+  }
+
+  const deleteData = (name) => {
+    setList(list.filter(o => o.name !== name));
+  }
+
+  const changeKeyword = (e) => {
+    setKeyword(e.target.value);
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input placeholder="이름" value={data.name} onChange={changeName}/>
+      <input placeholder="전화번호" value={data.phone} onChange={changePhone}/>
+      <button onClick={addContact}>등록</button>
+      <input placeholder="검색" value={keyword} onChange={changeKeyword}/>
+      {list
+      .filter(o => o.name.indexOf(keyword) > -1)
+      .map(item => (
+        <div key={item.name}>
+          <div>{item.name}</div>
+          <div>{item.phone}</div>
+          <button onClick={deleteData.bind(null, item.name)}>삭제</button>
+        </div>
+      ))}
     </div>
   );
 }
